f8295caaeb80154898442d6e7e60874032380eb6
zhumirov <zhumirov@mail.ru>
2020-08-24 13:03:06 +0600

    certificate updated

--- server/index.js
+++ server/index.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
+const privateKey  = fs.readFileSync('./selfsigned.key', 'utf8');
+const certificate = fs.readFileSync('./public-selfsigned.crt', 'utf8');
+
+
 const services = require('./services');
 const router = require('./routers');
 const { serverPort } = require('./config');
@@ -12,7 +19,10 @@ app
     .use(cors({credentials: true, origin: ['http://localhost:3333', 'http://ustaz.tilda.ws', 'http://nositel.kz'] }))
     .use('/api/v1', router);
 
-app.listen(serverPort, (error)=>{
+const credentials = {key: privateKey, cert: certificate};
+const httpsServer = https.createServer(credentials, app);
+
+httpsServer.listen(serverPort, (error)=>{
     if(error){
         console.error(error.message);
         return;
